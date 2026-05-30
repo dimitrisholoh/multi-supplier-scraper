d3f7dc1448b10add603fa5e9cbe615442a0f6809
dimitrisholoh <dimitri.sholoh@gmail.com>
2026-05-31 01:07:01 +0200

    36 - Test 10 Pages - Update scrape-julian.js

--- scrape-julian.js
+++ scrape-julian.js
@@ -6,7 +6,7 @@ const SUPPLIER_NAME = 'Julian Fashion Srl';
 const SUPPLIER_SLUG = 'julian-fashion';
 
 const LIMIT_PRODUCTS = Number(process.env.LIMIT_PRODUCTS || 48);
-const MAX_PAGES = Number(process.env.MAX_PAGES || 2);
+const MAX_PAGES = Number(process.env.MAX_PAGES || 10);
 
 const LISTING_URL = process.env.JULIAN_LISTING_URL || 'https://b2bfashion.online/306-all';
 

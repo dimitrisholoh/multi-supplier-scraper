ebb288f14924f1f38befb7644d58595b306712fe
dimitrisholoh <dimitri.sholoh@gmail.com>
2026-05-31 13:15:05 +0200

    37 Update scrape-julian.js

--- scrape-julian.js
+++ scrape-julian.js
@@ -5,8 +5,8 @@ const crypto = require('crypto');
 const SUPPLIER_NAME = 'Julian Fashion Srl';
 const SUPPLIER_SLUG = 'julian-fashion';
 
-const LIMIT_PRODUCTS = Number(process.env.LIMIT_PRODUCTS || 48);
-const MAX_PAGES = Number(process.env.MAX_PAGES || 10);
+const LIMIT_PRODUCTS = Number(process.env.LIMIT_PRODUCTS || 5);
+const MAX_PAGES = Number(process.env.MAX_PAGES || 1);
 
 const LISTING_URL = process.env.JULIAN_LISTING_URL || 'https://b2bfashion.online/306-all';
 

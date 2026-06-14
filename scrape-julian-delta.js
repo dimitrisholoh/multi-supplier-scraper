77adc20b8b263f5017ce5d664d226a1d41f1aa19
dimitrisholoh <dimitri.sholoh@gmail.com>
2026-06-14 22:21:06 +0200

    20 Pages Update scrape-julian-delta.js

--- scrape-julian-delta.js
+++ scrape-julian-delta.js
@@ -5,9 +5,9 @@ const crypto = require('crypto');
 const SUPPLIER_NAME = 'Julian Fashion Srl';
 const SUPPLIER_SLUG = 'julian-fashion';
 
-const LIMIT_PRODUCTS = Number(process.env.LIMIT_PRODUCTS || 10);
+const LIMIT_PRODUCTS = Number(process.env.LIMIT_PRODUCTS || 50);
 const START_PAGE = Number(process.env.START_PAGE || 1);
-const MAX_PAGES = Number(process.env.MAX_PAGES || 1);
+const MAX_PAGES = Number(process.env.MAX_PAGES || 20);
 
 const LISTING_URL = process.env.JULIAN_LISTING_URL || 'https://b2bfashion.online/306-all';
 
